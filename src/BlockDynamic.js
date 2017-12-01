b6a45400ceffb72dd610ab9af691f487ae062142
Robin de Mourat <robin.demourat@gmail.com>
2017-12-01 09:13:47 +0100

    fetch data from context datasets in a dynamic way

--- src/BlockDynamic.js
+++ src/BlockDynamic.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import VegaLite from 'react-vega-lite';
+import {get} from 'axios';
+import dsv from 'd3-dsv';
 
+class BlockDynamic extends Component {
 
-export default class Block extends Component {
-  constructor(props) {
+  static contextTypes = {
+    datasets: propTypes.object,
+  }
+
+  constructor (props) {
     super(props);
+  }
+
+  componentWillMount() {
+    this.updateData(this.props);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (
+        this.props.data !== nextProps.data ||
+        getDataset(this.props) !== getDataset(nextProps)
+      ) {
+      this.updateData(nextProps);
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -13,17 +33,72 @@ export default class Block extends Component {
       this.props.contextualizer.spec !== nextProps.contextualizer.spec
     )
   }
+
+  getDataset = (props) => {
+    return this.context && 
+           this.context.datasets && 
+           this.context.datasets[props.data.dataset]
+  }
+
+  formatData = (data, dataset) => {
+    switch(dataset.format) {
+      case 'csv':
+        return dsv.csvParse(data);
+      case 'tsv':
+        return dsv.tsvParse(data);
+      case 'json':
+      default:
+        return data;
+    }
+  }
+
+  updateData = (props) => {
+    const dataset = this.getDataset(props);
+    if (dataset === undefined) {
+      return;
+    }
+    if (dataset.rawData) {
+      this.setState({
+        loading: false,
+        data: dataset.rawData,
+        error: undefined,
+      })
+    } else if (dataset.uri) {
+      this.setState({
+        loading: true,
+        error: undefined,
+      });
+      axios.get(dataset.uri)
+      .then((response) => {
+        const data = this.formatData(response.data, dataset);
+        this.setState({
+          data,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error
+        })
+      });
+    } else {
+      this.setState({
+        error: 'no-dataset'
+      })
+    }
+  }
+
   render() {
     const {
-      resource: {
+      props: {
+        contextualizer: {
+          spec = {}
+        },
+      },
+      state: {
         data = []
-      },
-      contextualizer: {
-        spec = {
-        }
-      },
-      // contextualization
-    } = this.props;
+      }
+    } = this;
     let finalSpec;
     if ((!spec || !Object.keys(spec).length) && data.length) {
       const obj = data[0];
@@ -41,14 +116,23 @@ export default class Block extends Component {
           },
         }
       }
-    } else finalSpec = spec;
-    return <figure className="peritext-contextualization peritext-contextualization-block peritext-contextualization-web peritext-contextualizer-vegalite">
-              <VegaLite 
-                spec={finalSpec} 
-                data={{values: data}} 
-                renderer="svg"
-              />
-            </figure>
-
+    } else {
+      finalSpec = spec;
+    }
+    return (
+      <figure className="peritext-contextualization peritext-contextualization-block peritext-contextualization-web peritext-contextualizer-vegalite">
+        <VegaLite 
+          spec={finalSpec} 
+          data={{values: data}} 
+          renderer="svg"
+        />
+      </figure>
+    );
   }
 }
+
+
+
+export default BlockDynamic;
+
+
--- src/BlockStatic.js
+++ src/BlockStatic.js
@@ -1,37 +1,118 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import VegaLite from 'react-vega-lite';
+import {get} from 'axios';
+import dsv from 'd3-dsv';
 
+class BlockStatic extends Component {
 
-export default class Block extends Component {
-  constructor(props) {
+  static contextTypes = {
+    datasets: propTypes.object,
+  }
+
+  constructor (props) {
     super(props);
+  }
+
+  componentWillMount() {
+    this.updateData(this.props);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (
+        this.props.data !== nextProps.data ||
+        getDataset(this.props) !== getDataset(nextProps)
+      ) {
+      this.updateData(nextProps);
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     return (
       this.props.resource.data !== nextProps.resource.data ||
       this.props.contextualizer.spec !== nextProps.contextualizer.spec ||
-      this.props.contextualizer.thumbnailUrl !== nextProps.contextualizer.thumbnailUrl
+      this.props.contextualizer.thumbnailDataset !== nextProps.contextualizer.thumbnailDataset
     )
   }
+
+  getDataset = (props) => {
+    return this.context && 
+           this.context.datasets && 
+           this.context.datasets[props.data.dataset]
+  }
+
+  formatData = (data, dataset) => {
+    switch(dataset.format) {
+      case 'csv':
+        return dsv.csvParse(data);
+      case 'tsv':
+        return dsv.tsvParse(data);
+      case 'json':
+      default:
+        return data;
+    }
+  }
+
+  updateData = (props) => {
+    const dataset = this.getDataset(props);
+    if (dataset === undefined) {
+      return;
+    }
+    if (dataset.rawData) {
+      this.setState({
+        loading: false,
+        data: dataset.rawData,
+        error: undefined,
+      })
+    } else if (dataset.uri) {
+      this.setState({
+        loading: true,
+        error: undefined,
+      });
+      axios.get(dataset.uri)
+      .then((response) => {
+        const data = this.formatData(response.data, dataset);
+        this.setState({
+          data,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error
+        })
+      });
+    } else {
+      this.setState({
+        error: 'no-dataset'
+      })
+    }
+  }
+
   render() {
     const {
-      resource: {
+      props: {
+        contextualizer: {
+          spec = {},
+          thumbnailDataset
+        },
+      },
+      state: {
         data = []
       },
-      contextualizer: {
-        spec = {
-        },
-        thumbnailUrl
-      },
-      // contextualization
-    } = this.props;
-    let finalSpec;
-    if (thumbnailUrl) {
+      context: {
+        datasets
+      }
+    } = this;
+
+    const thumbnail = datasets[thumbnailDatasets];
+
+    if (thumbnail) {
       return (<figure className="peritext-contextualization peritext-contextualization-block peritext-contextualization-web peritext-contextualizer-vegalite">
-                <img className="contextualizer-thumbnail" src={thumbnailUrl} />
+                <img className="contextualizer-thumbnail" src={thumbnail.uri} />
             </figure>);
     }
+    let finalSpec;
     if ((!spec || !Object.keys(spec).length) && data.length) {
       const obj = data[0];
       const fields = Object.keys(obj);
@@ -48,14 +129,20 @@ export default class Block extends Component {
           },
         }
       }
-    } else finalSpec = spec;
-    return <figure className="peritext-contextualization peritext-contextualization-block peritext-contextualization-web peritext-contextualizer-vegalite">
-              <VegaLite 
-                spec={finalSpec} 
-                data={{values: data}} 
-                renderer="svg"
-              />
-            </figure>
+    } else {
+      finalSpec = spec;
+    }
+    return (
+      <figure className="peritext-contextualization peritext-contextualization-block peritext-contextualization-web peritext-contextualizer-vegalite">
+        <VegaLite 
+          spec={finalSpec} 
+          data={{values: data}} 
+          renderer="svg"
+        />
+      </figure>
+    );
 
   }
 }
+
+export default BlockStatic;
